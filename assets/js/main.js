a6100b111c5f886b19433d4d1363157d3815bbd1
samuelmahesasinulingga <sinulingga848@gmail.com>
2025-05-13 10:35:24 +0700

    update source code

--- assets/js/main.js
+++ assets/js/main.js
@@ -212,3 +212,13 @@ function toggleMobileMenu() {
   const navLinks = document.getElementById("nav-links");
   navLinks.classList.toggle("active");
 }
+
+
+  window.addEventListener('scroll', function () {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
